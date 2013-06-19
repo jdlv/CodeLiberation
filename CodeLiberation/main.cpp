6c5021569a0c0ba9f73fab63309d1f4efa8ea00c
Jennifer de la Vega <delavega@172-30-3-215.dynapool.nyu.edu>
2013-06-19 19:06:36 -0400

    Initial Commit

---
+++ CodeLiberation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CodeLiberation
+//
+//  Created by Jennifer de la Vega on 6/19/13.
+//  Copyright (c) 2013 Jenn de la Vega. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
