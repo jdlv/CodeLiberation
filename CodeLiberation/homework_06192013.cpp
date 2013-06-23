69a17dd36eae03981f7af16049ce278f0f727d21
jdlv <jenndlv@groupneon.com>
2013-06-23 07:33:17 -0400

    Musical chairs
    Beginning homework. So far you can enter your name. No game play yet.

--- CodeLiberation/homework_06192013.cpp
+++ CodeLiberation/homework_06192013.cpp
@@ -1,3 +1,4 @@
+
 //
 //  homework_06192013.cpp
 //  CodeLiberation
@@ -12,9 +13,68 @@ using namespace std;
 
 int main ()
 {
-    cout << "You win!" << endl;
-    return 0;
+    int name;
+    cout << "Healthy debate? What's your name?" << endl;
+    cin >> name;
+    
+    cout << "Let's rumble." << endl;
+    
+    int choice;
+    cout << "Choice:";
+    cin >> choice;
+    
+    switch (choice) {
+            
+        case 1:
+            cout << "Intimidate!";
+            break;
+            
+        case 2:
+            cout << "Listen...and think of something witty to say!";
+            break;
+            
+        default:
+            cout << "Moot point, move on...";
+            break;
+    }
+    
+    if (true) {
+        cout << "";
+    }
+    if (false) {
+        
+        cout << "";
+    }
+    
 }
 
-cout << "You Lose!" << endl;
 
+Player Kay: 100
+Monster: 100
+Choose an Action:
+1) Attack
+2) Defend
+( Player types in 1 )
+You choose to attack. Monster takes 20 damage. You take 10 damage.
+
+Player Kay: 90
+Monster: 80
+Choose an Action:
+1) Attack
+2) Defend
+( Player types in 2 )
+You choose to defend. Monster takes 0 damage. You take 5 damage.
+
+(Continues until last round)
+
+Player Kay: 60
+Monster: 20
+Choose an Action:
+1) Attack
+2) Defend
+( Player types in 1 )
+You choose to attack. Monster takes 20 damage. You take 10 damage.
+
+Player Kay: 50
+Monster: 0
+Player Kay wins! End of game.
