ac9fe5b5129bd70028a11920f4396fc7504e7aed
jdlv <jenndlv@groupneon.com>
2013-06-25 19:26:13 -0400

    Added switch cases
    You can now pick to attack or defend. But no loop yet.

--- CodeLiberation/main.cpp
+++ CodeLiberation/main.cpp
@@ -14,8 +14,29 @@ int main ()
     string pokemon;
     cout << "A wild Pikachu appears! Which Pokemon do you send out?" << endl;
     cin >> pokemon;
+
+    int playerLife;
+    int enemyLife;
+    int choice;
+    
+    playerLife = 100;
+    enemyLife = 100;
     
     cout << "\nYou send out " << pokemon << endl;
-    cout << "Pikachu: 100" << endl;
-    cout << pokemon << ": 100" << endl;
+    cout << "Pikachu HP: " << enemyLife << endl;
+    cout << pokemon << " HP: " << playerLife << endl;
+    cout << "Type 1 to attack or 2 to defend" << endl;
+    cin >> choice;
+    
+    switch (choice)
+    {
+        case 1:
+            cout << "Your " << pokemon << " attacked the wild Pikachu and took 10HP of damage!" << endl;
+            break;
+        case 2:
+            cout << "Your " << pokemon << " defended itself and took 5HP of damage from the wild Pikachu!" << endl;
+        default:
+            cout << "The wild Pikachu is getting antsy, tell your " << pokemon << "to attack or defend!" << endl;
+    }
 }
+
