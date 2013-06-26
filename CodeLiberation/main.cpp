274c74c17ac2b844f91d0d8de522e2ae381fc59b
jdlv <jenndlv@groupneon.com>
2013-06-25 21:02:37 -0400

    Moved things around
    Still having trouble keeping track of HP variable

--- CodeLiberation/main.cpp
+++ CodeLiberation/main.cpp
@@ -31,13 +31,13 @@ int main ()
     switch (choice)
     {
         case 1:
-            cout << "Your " << pokemon << " attacked the wild Pikachu and took 10 HP of damage!" << endl;
+            cout << "\nYour " << pokemon << " attacked the wild Pikachu and took 10 HP of damage!" << endl;
             break;
         case 2:
-            cout << "Your " << pokemon << " defended itself and took 5 HP of damage from the wild Pikachu!" << endl;
+            cout << "\nYour " << pokemon << " defended itself and took 5 HP of damage from the wild Pikachu!" << endl;
             break;
         default:
-            cout << "The wild Pikachu is getting antsy, tell your " << pokemon << " to attack or defend!" << endl;
+            cout << "\nThe wild Pikachu is getting antsy, tell your " << pokemon << " to attack or defend!" << endl;
     }
         do
     {
@@ -45,31 +45,34 @@ int main ()
         {
             cout << "Pikachu HP: " << enemyLife - 25 << endl;
             cout << pokemon << " HP: " << playerLife - 10 << endl;
-            cout << "The battle wages on! Press 1 to attack or 2 to defend!" << endl;
+            cout << "\nThe battle wages on! Press 1 to attack or 2 to defend!" << endl;
             cin >> choice;
         }
         else if (choice == 2 && enemyLife && playerLife > 0)
         {
             cout << "Pikachu HP: " << enemyLife << endl;
             cout << pokemon << " HP: " << playerLife - 5 << endl;
-            cout << "The battle wages on! Press 1 to attack or 2 to defend!" << endl;
+            cout << "\nThe battle wages on! Press 1 to attack or 2 to defend!" << endl;
             cin >> choice;
         }
         else if (enemyLife && playerLife > 0)
         {
             cout << "Pikachu HP:" << enemyLife << endl;
             cout << pokemon << " HP: " << playerLife << endl;
-            cout << "The battle wages on! Press 1 to attack or 2 to defend!" << endl;
+            cout << "\nThe battle wages on! Press 1 to attack or 2 to defend!" << endl;
             cin >> choice;
         }
-        else if (enemyLife <= 0)
+    } while (enemyLife && playerLife > 0);
+    
+    if (enemyLife <= 0)
     {
         cout << "The wild Pikachu has fainted! You win!";
-    }
-        else if (playerLife <= 0)
     
-    {
-        cout << "Your " << pokemon << " has fainted and the wild Pikachu has run away! You lose!" << endl; 
     }
-    } while (enemyLife && playerLife > 0);
+        else
+    {
+        cout << "Your " << pokemon << " has fainted and the wild Pikachu has run away! You lose!" << endl;
+    }
+ 
+    
 }
